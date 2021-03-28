3968407b44eb42cd8b7bb83ad0866948b9b6a765
Alan Duong <alanduong07@gmail.com>
2021-03-28 14:35:54 -0500

    Add files via upload

--- pdfplumbercurve.py
+++ pdfplumbercurve.py
@@ -4,6 +4,8 @@ import pdfplumber
 #report = pdfplumber.open("C:/Users/aland/Documents/Rice Documents/Rice Freshman Year/PDF Parsing/lswagenergy.pdf").pages[0]
 report = pdfplumber.open("C:/Users/aland/Documents/Rice Documents/Rice Freshman Year/PDF Parsing/bubbleballot01.pdf").pages[0]
 im = report.to_image()
-im
-print(len(report.curves))
-print(report.curves[12])
+im.save("C:/Users/aland/Documents/Rice Documents/Rice Freshman Year/PDF Parsing/", format="PNG")
+
+# print(im.draw_lines(report.curves, stroke="red", stroke_width=2))
+# print(len(report.curves))
+# print(report.curves[12])
