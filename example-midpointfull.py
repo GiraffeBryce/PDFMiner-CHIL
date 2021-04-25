67da20690661924444452256b5f68c1fdfe4bc1e
arixwang <46141311+arixwang@users.noreply.github.com>
2021-04-25 14:28:24 -0500

    Add files via upload
    Added comments

--- example-midpointfull.py
+++ example-midpointfull.py
@@ -2,46 +2,33 @@ import numpy as np
 import pdfplumber
 
 # Loading the file
-sample_ballot = "/Users/arianawang/Documents/Rice/CHIL bubbleballot01 (1).pdf"
-
-# Looking at the first few rows
-with pdfplumber.open(sample_ballot) as pdf:
-    first_page = pdf.pages[0]
-    rows = first_page.extract_text().split('\n')
-rows[:10]
-
-# print("rows", rows)
-
-# print(first_page.to_image(resolution=150).outline_words())
-
-
-#report = pdfplumber.open("C:/Users/aland/Documents/Rice Documents/Rice Freshman Year/PDF Parsing/lswagenergy.pdf").pages[0]
+sample_ballot = "/Users/arianawang/Documents/Rice/Sample/CHIL bubbleballot01 (1).pdf"
+# Choosing page of ballot to run the code
 report = pdfplumber.open(sample_ballot).pages[0]
-report.curves[12]
+# Create the image to save
 im = report.to_image()
-im.draw_lines(report.curves, stroke="red", stroke_width=2)
-# im.save("/Users/arianawang/Documents/Rice/CHIL bubbleballot01 (3).png", format="PNG")
 
-
-im.reset()
+# Colors that the coordinates will be mapped in
 colors = [ "gray", "red", "blue", "green" ]
+
+# Iterate through the detected curves
 for i, curve in enumerate(report.curves):
-    stroke = colors[i%len(colors)]
-    x_values = 0
-    y_values = 0
-    for points in curve["points"]:
-        x_values += points[0]
-        y_values += points[1] 
-    midpoint = [(curve["width"]/2 + curve["x0"], curve["height"]/2 + curve["y0"])]
-    midpoint = [(round((x_values/len(curve["points"]))), round((y_values/len(curve["points"]))))]
-    print("midpoint", midpoint)
-    im.draw_circles(midpoint, radius=3, stroke=stroke, fill="white")
-    # im.draw_line(curve["points"], stroke=stroke, stroke_width=2)
+    # If the curve detected is not a line (i.e. two points on the curve detected)
+    if len(curve["points"]) != 2:
+        # The color chosen
+        stroke = colors[i%len(colors)]
+        x_values = 0
+        y_values = 0
+        # Sums up the values of the curves
+        for points in curve["points"]:
+            x_values += points[0]
+            y_values += points[1] 
+        # Calculates the midpoint of the bubble
+        midpoint = [(round((x_values/len(curve["points"]))), round((y_values/len(curve["points"]))))]
+        # Marks the curve
+        im.draw_circles(midpoint, radius=3, stroke=stroke, fill="white")
 
-im.save("/Users/arianawang/Documents/Rice/CHIL bubbleballot01 TEST5.png", format="PNG")
-
-# print(im.draw_lines(report.curves, stroke="red", stroke_width=2))
-# print(len(report.curves))
-# print(report.curves[12])
+# Saves the new ballot
+im.save("/Users/arianawang/Documents/Rice/Sample/CHIL bubbleballot01 testthree.png", format="PNG")
 
 # Github link: https://github.com/jsvine/pdfplumber/blob/stable/examples/notebooks/ag-energy-roundup-curves.ipynb
